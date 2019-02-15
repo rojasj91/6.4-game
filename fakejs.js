372e0213a3700423caba116f109d24998edd8007
Christina <Christinepeninsula@hotmail.com>
2019-02-15 12:52:19 -0500

    Setting up api and other apps

--- fakejs.js
+++ fakejs.js
@@ -5,18 +5,14 @@ class Characters {
     constructor(context) {
         charName = context.charName;
         health = 100;
-        id = context.id
+        id = context.id;
 
-
+        //do we need to define changed
         attack(selectedEnemy){
             selectedEnemy.health = selectedEnemy.health - this.attack;
             $(document).trigger('health:changed');
         }
 
-         counter(selectedCharacter){
-            selectedCharacter.health = selectedCharacter.health - this.counter;
-            $(document).trigger('health:changed');
-        }
     }
 };
 
@@ -41,13 +37,13 @@ class Opponent extends Characters {
 
 // id for each each character, will use random math onl;y for char 4-6 to choose enemy
 // what health is really doing in object
-var foxy = new Chiahuahua({ charName: 'Foxy', health: '', attack: 19, id: 1});
-var paco = new Chiahuahua({ charName: 'Paco', health: '', attack: 18, id: 2});
-var karen = new Chiahuahua({charName: 'Karen', health: '', attack:13, id: 3 });
+var foxy = new Chiahuahua({ charName: 'Foxy', attack: 19, id: 1});
+var paco = new Chiahuahua({ charName: 'Paco', attack: 18, id: 2});
+var karen = new Chiahuahua({charName: 'Karen', attack:13, id: 3 });
 
-var squirrel = new Opponent({ charName: 'Sandy', health: '', attack: 12, id: 4});
-var opossum = new Opponent({charName: 'MoonShine', health: '', attack:17, id: 5});
-var rat = new Opponent({charName:'Splinter', health: '', attack: 10, id: 6});
+var squirrel = new Opponent({ charName: 'Sandy', attack: 12, id: 4});
+var opossum = new Opponent({charName: 'MoonShine', attack:17, id: 5});
+var rat = new Opponent({charName:'Splinter', attack: 10, id: 6});
 
 //index.js $('.attack-button').on('click', function(e) { if(itsMyTurn){ $(document).trigger('attack:enemy'); } });
 
@@ -60,9 +56,43 @@ var rat = new Opponent({charName:'Splinter', health: '', attack: 10, id: 6});
 
 
 
-
+// is selected built in
 $(document).on('character:selected', function(event, character) { selectedCharacter = character; });
 
 $(document).on('enemy:selected', function(event, enemy) { selectedEnemy = enemy; });
 
 $(document).on('attack:enemy', function(event) { selectedCharacter.attack(selectedEnemy); });
+
+
+function counter(y, c) {
+	var move = Math.floor((Math.random()*5));
+	if (move >= 3 && y === 'attack') {
+		res = 'Computers counter was successful! You took 10 damage';
+		yourHealth -= 10;
+	} else if (move >= 3 && y === 'counter') {
+		res = 'Your counter was successful! Comp took 10 damage';
+		compHealth -= 10;
+	} else if (move < 3 && y === 'attack') {
+		res = 'Computer counter failed! You dealt 15 damage!';
+		compHealth -= 15;
+	} else if (move < 3 && y === 'counter') {
+		res = 'Your counter was not successful! You were delalt 15 damage!';
+		yourHealth -= 15;
+	}
+
+}
+
+
+
+//html elements need to add, attack button, play again button,
+ var result = ""
+
+function gameOver() {
+	if (health === 0) {
+		res = 'gameOver!';
+		roundResults(res);
+		attackButton.disabled = true;
+		// counterButton.disabled = true;
+		playAgain.disabled = true;
+	}
+}
---
+++ models.js
@@ -0,0 +1,5 @@
+
+
+
+var selectedCharacter = ''
+var selectedEnemy = ''
